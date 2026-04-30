67d10ad770db0a6747474bd88ee200feefacaf0b
Giovanni Zampetti <giovannizampetti08@gmail.com>
2026-04-30 16:31:08 +0200

    add astro adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
   integrations: [],
 
@@ -11,5 +13,7 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
